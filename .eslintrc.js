f29218e16a5c23281e26a21a71285a99262ad9f3
umatoma <umatomakun@gmail.com>
2016-10-12 22:33:29 +0900

    refactoring

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "globals": {},
+  "extends": "airbnb-base",
+  "rules": {
+    "strict": "off",
+    "comma-dangle": ["error", "never"],
+    "prefer-rest-params": "off"
+  }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -10,73 +10,8 @@ const watch = require('gulp-watch');
 
 // remove --gulpfile and --cwd options
 const argv = require('./lib/cmd-args')(process.argv.slice(0, -4));
+
 const DEBUG_MODE = (argv.debug === true);
-
-/**
- * Watch files and exec rsync command
- *
- * Command:
- *   $ rst watch
- */
-gulp.task('watch', (done) => {
-  let config = getConfigFromFile();
-
-  return watch(config.files, (vinyl) => {
-    gutil.log('File ' + vinyl.path + ' was ' + vinyl.event + ', running tasks...');
-
-    return execRsync(config, (err, code, cmd) => {
-      if (err) {
-        gutil.log('[ERROR]', err.message)
-      }
-      gutil.log('[CODE]', code);
-      gutil.log('[COMMAND]', cmd);
-    });
-  });
-});
-
-/**
- * Synchronize the local to the server
- *
- * Command:
- *   $ rst sync-local
- */
-gulp.task('sync-local', (done) => {
-  let config = getConfigFromFile();
-
-  return execRsync(config, (err, code, cmd) => {
-    if (err) {
-      gutil.log('[ERROR]', err.message)
-    }
-    gutil.log('[CODE]', code);
-    gutil.log('[COMMAND]', cmd);
-
-    done();
-  });
-});
-
-/**
- * Synchronize the server to the local
- *
- * Command:
- *   $ rst sync-remote
- */
-gulp.task('sync-remote', (done) => {
-  let config = getConfigFromFile();
-  let source = _.clone(config.destination);
-  let destination = _.clone(config.source);
-  config.source = source;
-  config.destination = destination;
-
-  return execRsync(config, (err, code, cmd) => {
-    if (err) {
-      gutil.log('[ERROR]', err.message)
-    }
-    gutil.log('[CODE]', code);
-    gutil.log('[COMMAND]', cmd);
-
-    done();
-  });
-});
 
 /**
  * Get config from json file.
@@ -84,25 +19,25 @@ gulp.task('sync-remote', (done) => {
  * @return {object} - Config object
  */
 function getConfigFromFile() {
-  let config_path = path.resolve(process.cwd(), '.rsync-tool.json');
-  let user_config = JSON.parse(fs.readFileSync(config_path));
-  let config = {
+  const configPath = path.resolve(process.cwd(), '.rsync-tool.json');
+  const userConfig = JSON.parse(fs.readFileSync(configPath));
+  const config = {
     shell: 'ssh',
     flags: 'azr',
     files: ['**/*', '!.rsync-tool.json', '!.git'],
     exclude: ['.rsync-tool.json', '.git'],
-    source: process.cwd() + '/',
+    source: `${process.cwd()}/`,
     destination: null
   };
 
   // add exclude files
-  if (user_config.hasOwnProperty('exclude')) {
-    config.exclude = config.exclude.concat(user_config.exclude);
-    config.files = config.files.concat(config.exclude.map( v => '!' + v ));
+  if (_.has(userConfig, 'exclude')) {
+    config.exclude = config.exclude.concat(userConfig.exclude);
+    config.files = config.files.concat(config.exclude.map(v => `!${v}`));
   }
 
   // merge user config
-  _.merge(config, _.pick(user_config, [
+  _.merge(config, _.pick(userConfig, [
     'shell',
     'flags',
     'source',
@@ -119,11 +54,11 @@ function getConfigFromFile() {
  * @param {function} callback - Callback function
  */
 function execRsync(config, callback) {
-  let rsync = new Rsync()
+  const rsync = new Rsync()
     .shell(config.shell)
     .flags(config.flags)
     .set('progress')
-    .delete()
+    .deconste()
     .exclude(config.exclude)
     .source(config.source)
     .destination(config.destination);
@@ -141,3 +76,69 @@ function execRsync(config, callback) {
       }
     });
 }
+
+/**
+ * Watch files and exec rsync command
+ *
+ * Command:
+ *   $ rst watch
+ */
+gulp.task('watch', () => {
+  const config = getConfigFromFile();
+
+  return watch(config.files, (vinyl) => {
+    gutil.log(`File ${vinyl.path} was ${vinyl.event}, running tasks...`);
+
+    return execRsync(config, (err, code, cmd) => {
+      if (err) {
+        gutil.log('[ERROR]', err.message);
+      }
+      gutil.log('[CODE]', code);
+      gutil.log('[COMMAND]', cmd);
+    });
+  });
+});
+
+/**
+ * Synchronize the local to the server
+ *
+ * Command:
+ *   $ rst sync-local
+ */
+gulp.task('sync-local', (done) => {
+  const config = getConfigFromFile();
+
+  return execRsync(config, (err, code, cmd) => {
+    if (err) {
+      gutil.log('[ERROR]', err.message);
+    }
+    gutil.log('[CODE]', code);
+    gutil.log('[COMMAND]', cmd);
+
+    done();
+  });
+});
+
+/**
+ * Synchronize the server to the local
+ *
+ * Command:
+ *   $ rst sync-remote
+ */
+gulp.task('sync-remote', (done) => {
+  const config = getConfigFromFile();
+  const source = _.clone(config.destination);
+  const destination = _.clone(config.source);
+  config.source = source;
+  config.destination = destination;
+
+  return execRsync(config, (err, code, cmd) => {
+    if (err) {
+      gutil.log('[ERROR]', err.message);
+    }
+    gutil.log('[CODE]', code);
+    gutil.log('[COMMAND]', cmd);
+
+    done();
+  });
+});
